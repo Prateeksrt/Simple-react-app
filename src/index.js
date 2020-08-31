0c3cf156efd6d5f6d5912f562b38f272d740032d
Prateek Srivastava <Prateek.Srivastava@agoda.com>
2020-08-31 17:56:37 +0700

    removed strict mode

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
     {/*new component goes here*/}
     <h1>Hello world</h1>
-  </React.StrictMode>,
+  </>,
   document.getElementById('root')
 );
